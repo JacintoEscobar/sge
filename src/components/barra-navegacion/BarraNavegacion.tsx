d0f191be31302e3b428cad418c996f46bd23178c
Jacinto Escobar <eqjo190657@upemor.edu.mx>
2024-03-21 12:25:39 -0700

    crear la barra de navegacion

---
+++ src/components/barra-navegacion/BarraNavegacion.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+import "./barra-navegacion.css"
+
+const BarraNavegacion = () => {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">
+                    <img id="barra-navegacion-logo" src="img/sge-logo.png" alt="sge" className="d-inline-block align-text-top" />
+                    <span id="barra-navegacion-titulo">SGE</span>
+                </Link>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default BarraNavegacion;
